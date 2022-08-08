5197f55214dae335f8b62318ac3830d1dd09c5ff
Deepjyoti Khatoniar <59142699+dkhato@users.noreply.github.com>
2022-08-08 10:49:56 +0530

    Add files via upload

---
+++ programming_assignment_1.py
@@ -0,0 +1,33 @@
+# Program 1
+print("****Printing program 1--****")
+print("Hello Python")
+
+# Program 2
+print("****Printing program 2 -- Arithmatic ops****")
+a = 2
+b = 2
+c = a + b
+d = c / a
+print(c)
+print(d)
+
+# Program 3
+print("****Printing program 3 -- the area of a triangle****")
+b = int(input("Enter base:"))
+h = int(input("Enter Height:"))
+a = (b * h) / 2
+print(a)
+
+# Program 4
+print("****Printing program 4 -- Swapping two variables****")
+a = 3
+b = 2
+c = a
+a = b
+b = c
+print(a)
+print(b)
+
+print("****Printing program 5 -- Generating randon number****")
+import random
+print(random.randint(0, 100))
